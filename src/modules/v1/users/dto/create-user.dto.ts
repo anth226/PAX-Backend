298e5945e67a267010682c0ba8b028688af27b02
endlessorhub <anthonyhutchi99@gmail.com>
2023-07-03 23:35:18 +0545

    auth fix

--- src/modules/v1/users/dto/create-user.dto.ts
+++ src/modules/v1/users/dto/create-user.dto.ts
@@ -10,7 +10,7 @@ export class CreateUserDto {
 
   @ApiProperty({description: "Login Password", example: "123456aA#"})
   @IsString()
-  @PasswordComplexity()
+  // @PasswordComplexity()
   readonly password: string;
 
   @ApiProperty({description: "Phone Number", example: "+1XXXXXXXXX"})
